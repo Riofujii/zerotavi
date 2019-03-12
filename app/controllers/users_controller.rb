dc136b0519826e3ca6f5a4a00969ce93c46b2195
EC2 Default User <ec2-user@ip-172-31-2-113.us-east-2.compute.internal>
2019-03-12 14:33:45 +0000

    Edit search function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,6 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
-    @microposts = Microposts.search(params[:search]).paginate(page: params[:page])
   end
   
   def index
