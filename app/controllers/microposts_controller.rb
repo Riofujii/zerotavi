3714bc42ad0d54ef15bbd44d3de12ba91a398503
ymachida <yasnori1118@gmail.com>
2019-03-17 07:22:45 +0000

    finish micropost-detail

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -3,4 +3,8 @@ class MicropostsController < ApplicationController
   def index
     @microposts = Micropost.search(params[:search]).paginate(page: params[:page], per_page: 15)
   end
+  
+  def show
+    @micropost = Micropost.find(params[:id])
+  end
 end
